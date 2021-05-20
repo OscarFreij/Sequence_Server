bd527fe269097b667fb6b79c570b144c8f328ecb
oscar <oscar@OTGCG-Server-1.OTGCG-Server>
2021-05-21 01:02:51 +0200

    added text to home and about pages

---
+++ private/pages/about.php
@@ -0,0 +1,10 @@
+<div class="container mt-3">
+    <h1 class="text-center fs-1 text mb-4">About the game and me!</h1>
+    <p class="text-center fs-4 text">My name is Oscar, I also go by the name of OffTheGridCG.</p>
+    <p class="text-center fs-4 text">This game is a fun project I started working on during a stream and it has since become one of my biggest projects I have done.</p>
+    <p class="text-center fs-4 text">The game, server and webpage has taken roughly 34 hours of work and has been a blast do make.</p>
+    <p class="text-center fs-3 text">I hope you enjoy it!</p>
+</div>
+<div class="container">
+    <h3 class="text-center fs-3 text">Get the game here: <a href="https://github.com/OscarFreij/Sequence">On Github</a></h3>
+</div>
---
+++ private/pages/home.php
@@ -0,0 +1,13 @@
+<div class="container mt-3">
+    <h1 class="text-center fs-1 text">Sequence : The scoreboard</h1>
+    <h3 class="text-center fs-3 text">Get the game here: <a href="https://github.com/OscarFreij/Sequence">On Github</a></h3>
+</div>
+
+<div class="container mt-5">
+    <h2 class="text-center fs-2 text">Scores</h2>
+    <div class="gap-2 row">
+        <a class="col fs-3 btn btn-danger" href="http://sequence.offthegridcg.me/?page=score&diff=fast">Fast</a>
+        <a class="col fs-3 btn btn-warning" href="http://sequence.offthegridcg.me/?page=score&diff=norm">Normal</a>
+        <a class="col fs-3 btn btn-success" href="http://sequence.offthegridcg.me/?page=score&diff=slow">Slow</a>
+    </div>
+</div>
